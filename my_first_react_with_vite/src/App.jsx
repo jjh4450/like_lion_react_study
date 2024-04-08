c87f4217ca3af9ae3f9a12ee50362c3757b80b66
JunHWan Jo (jeje) <jjh4450git@gamil.com>
2024-04-09 08:54:00 +0900

    Post 컴포넌트 다루기

--- my_first_react_with_vite/src/App.jsx
+++ my_first_react_with_vite/src/App.jsx
@@ -3,31 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Post() {
+  return(
+    <div className='post'>
+      <div>
+        <img src='https://media.licdn.com/dms/image/D5603AQHgVncQ45cPvw/profile-displayphoto-shrink_800_800/0/1698215501617?e=1718236800&v=beta&t=wCgZP0rahoAN2F3BKnH5VL4fTTIv6QmEe3XRFbJi0yQ'/>
+        <li>
+          내 손끝에서 무언가가 만들어지고, 이를 통해 다른 사람을 지탱할 수 있기를 바라는 개발자입니다.
+        </li>
+      </div>
+      <p/>
+      좋아요: 5
+      <br/>
+      댓글 5개
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
     </>
   )
 }
